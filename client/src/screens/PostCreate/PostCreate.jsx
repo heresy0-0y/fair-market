20cf745b5890ab6a69b6d26bcb8785fef91c2054
Gigi <gigi@Gigis-MacBook-Pro.local>
2021-02-10 15:48:01 -0500

    Screens complete

---
+++ client/src/screens/PostCreate/PostCreate.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import "./PostCreate.css";
+import Layout from "../../components/shared/Layout/Layout";
+import { Redirect } from "react-router-dom";
+import { createPost } from "../../services/posts";
+
+const PostCreate = (props) => {
+  const [post, setPost] = useState({
+    subject: "",
+    content: "",
+    
+  })
+  const [isCreated, setCreated] = useState(false)
+
+  const handleChange = (event) => {
+    const { subject, value } = event.target;
+    setPost({
+      ...post,
+      [subject]: value,
+    })
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const created = await createPost(post);
+    setCreated({ created });
+  };
+  if (isCreated) {
+    return <Redirect to={`/posts`} />;
+  }
+  return (
+    <Layout user={props.user}>
+      <form className="create-form" onSubmit={handleSubmit}>
+        <input
+          className="input-subject"
+          placeholder="Subject"
+          value={props.user}
+          name="subject"
+          required
+          autoFocus
+          onChange={handleChange}
+        />
+        <textarea
+          className="textarea-content"
+          rows={10}
+          placeholder='Content'
+          value={post.content}
+          name='content'
+          required
+          onChange={handleChange}
+        />
+      </form>
+    </Layout>
+  );
+}
+export default PostCreate
