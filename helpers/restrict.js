049b6e16ac60ca3440c28b8f1de91213d198ad1b
hera <hera@heras-iMac.local>
2021-02-10 17:04:43 -0600

    users in backend and rendering in page/ layout components done

--- helpers/restrict.js
+++ helpers/restrict.js
@@ -5,7 +5,7 @@ const restrict = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
     const data = jwt.verify(token, TOKEN_KEY);
-    // res.locals.user = data
+    res.locals.user = data;
     next();
   } catch (error) {
     console.log(error);
